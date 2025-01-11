0f2c667a57bbb711a957a8b2f0f0d0d54b3124a7
Téo Calvo <teocalvo2@gmail.com>
2025-01-11 00:14:54 -0300

    feat: for loop to execute .sh

--- exec.sh
+++ exec.sh
@@ -1,55 +1,11 @@
-rm results.txt;
+rm results.txt
 
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_csv;
+for i in {1..12}
+do
+    python -m cudf.pandas exec.py --exec_type cudf_pandas_csv
+    python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet
+    python exec.py --exec_type pandas_parquet
+    python exec.py --exec_type pandas_csv
+done
 
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-python -m cudf.pandas exec.py --exec_type cudf_pandas_parquet;
-
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-python exec.py --exec_type pandas_parquet;
-
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-python exec.py --exec_type pandas_csv;
-
-python analysis.py;
+python analysis.py
