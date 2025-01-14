4edc49034539496d91b3d98a665dc248653fa541
Teo Calvo <teocalvo2@gmail.com>
2025-01-14 10:16:56 -0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -123,120 +123,120 @@ done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 115000000
+    python exec.py --engine pandas --file_format parquet --size 115000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 110000000
+    python exec.py --engine pandas --file_format parquet --size 110000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 105000000
+    python exec.py --engine pandas --file_format parquet --size 105000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 100000000
+    python exec.py --engine pandas --file_format parquet --size 100000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 95000000
+    python exec.py --engine pandas --file_format parquet --size 95000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 90000000
+    python exec.py --engine pandas --file_format parquet --size 90000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 85000000
+    python exec.py --engine pandas --file_format parquet --size 85000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 80000000
+    python exec.py --engine pandas --file_format parquet --size 80000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 75000000
+    python exec.py --engine pandas --file_format parquet --size 75000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 70000000
+    python exec.py --engine pandas --file_format parquet --size 70000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 65000000
+    python exec.py --engine pandas --file_format parquet --size 65000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 60000000
+    python exec.py --engine pandas --file_format parquet --size 60000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 55000000
+    python exec.py --engine pandas --file_format parquet --size 55000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 50000000
+    python exec.py --engine pandas --file_format parquet --size 50000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 45000000
+    python exec.py --engine pandas --file_format parquet --size 45000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 40000000
+    python exec.py --engine pandas --file_format parquet --size 40000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 35000000
+    python exec.py --engine pandas --file_format parquet --size 35000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 30000000
+    python exec.py --engine pandas --file_format parquet --size 30000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 25000000
+    python exec.py --engine pandas --file_format parquet --size 25000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 20000000
+    python exec.py --engine pandas --file_format parquet --size 20000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 15000000
+    python exec.py --engine pandas --file_format parquet --size 15000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 10000000
+    python exec.py --engine pandas --file_format parquet --size 10000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 5000000
+    python exec.py --engine pandas --file_format parquet --size 5000000
 done
 
 for i in {1..12}
 do
-    python -m --engine pandas --file_format parquet --size 1000000
+    python exec.py --engine pandas --file_format parquet --size 1000000
 done
